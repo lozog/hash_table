6415e1d72a73e8fde1e5eb9b09186288f7bac88e
lozog <liam.ozog@gmail.com>
2018-07-28 00:46:53 -0400

    hash table - collisions are overwritten

--- hash_chaining.py
+++ hash_chaining.py
@@ -1,4 +1,24 @@
 class HashTable:
-	def __init__(self):
-		self.data = []
 
+	def __init__(self, size = 101):
+		self.data = [0 for _ in range(size)]
+		self.size = size
+
+	# returns the hashed value of the item
+	# i.e. the index the item will be stored at
+	def hash_val(self, item):
+		return hash(item) % self.size
+
+	# insert item into hash table
+	def insert(self, item):
+		hash_val = self.hash_val(item)
+		self.data[hash_val] = item
+
+	# returns true if item is in hash table
+	def search(self, item):
+		hash_val = self.hash_val(item)
+		return self.data[hash_val] == item
+
+hashTable = HashTable()
+hashTable.insert('1000')
+print(hashTable.search('100'))
